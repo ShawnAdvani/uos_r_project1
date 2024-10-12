7bbcd57b4b08a24aa3751996cfa31c5146391b92
Shawn Advani <54037408+ShawnAdvani@users.noreply.github.com>
2024-10-12 10:41:59 +0100

    add code worked on in class

--- project1.R
+++ project1.R
@@ -10,29 +10,51 @@
 
 # Remember to load the tidyverse library
 
-
+library(tidyverse)
+library(glue)
 
 # load the data in the csv file into R (as a dataframe)
 
+df <- read.csv("data/anscombe.csv")
 
 # you can see the top of a large dataframe using the head command
 
+head(df)
+
 # you can look at a single column (also known as "indexing") by using the $ notation
+
+x1 <- df$x1
 
 # find the mean of one of the columns
 
+x1_mean <- mean(df$x1)
+
 # find the standard deviation of one of the columns
+
+x1_sd <- sd(df$x1)
 
 # find the correlation of one of the pairs
 
+cor_x1_y1 <- cor(df$x1, df$y1)
 
 # ADVANCED: find the mean and sd of ALL columns
 
-
+df %>% summarise(across(X:y4, mean))
+df %>% summarise(across(X:y4, sd))
 
 ## Now plot the data!
 
 # use ggplot to make a scatterplot
+
+df_x <- c("x1","x2","x3","x4")
+df_y <- c("y1","y2","y3","y4")
+
+for (i in 1:4) {  # Loop through count of x and y values
+  ggplot(data = df, mapping = aes(x=df_x[i], y=df_y[i])) +
+    geom_point(color=df$X) +
+    labs(title = glue('{df_x[i]} by {df_y[i]}'))
+}
+
 # hint: this will require two lines, one of which will involve the geom_point function
 
 # give the plot a title
